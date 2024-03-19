4cd7f3f8fb8a9aed6fdcad2d33f0f1da4ff6a4e3
abcamuez <82919641+abcamuez@users.noreply.github.com>
2024-03-19 06:04:31 -0400

    exercises

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Basketball</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Basketball was originally played with a soccer ball.</li>
+            <li>Harlem Globetrotters has been around since 1926.</li>
+            <li>Tallest NBA Player Was 7-foot-7-inches and the Shortest was 5-foot-3-inches.</li>
          </ol>
       </div>
    );
